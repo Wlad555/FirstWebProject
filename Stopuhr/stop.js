8c7499820aba87684c43130e7406ea06f5cc02e7
Wlad555 <102764055+Wlad555@users.noreply.github.com>
2023-01-06 01:17:24 +0100

    Fertige Stoppuhr

--- Stopuhr/stop.js
+++ Stopuhr/stop.js
@@ -2,16 +2,49 @@ let startZeit = 0;
 let endZeit = 0;
 let output = document.getElementById("ZeitWert");
 let intervalId;
+let speicher = [];
+
+let running = false;
 
 const starteZeit = () => {
+  if (running) {
+    // Schalter mit boolean
+    return;
+  }
   startZeit = Date.now();
-
   intervalId = setInterval(() => {
-    endZeit = (Date.now() - startZeit) / 1000;
-    output.innerHTML = endZeit;
+    endZeit = Date.now() - startZeit;
+    output.innerHTML = formatTime(endZeit);
   }, 1);
+  running = true;
 };
 
 const stoppeZeit = () => {
+  running = false; // Schalter arbeitet mit oben zsm.
   clearInterval(intervalId);
+  if ((speicher.length = 2)) {
+    speicher.unshift(endZeit);
+    
+  }
+  let speicherung = document.getElementById("Speicher");
+  let werte = "";
+  for (let i = 0; i < speicher.length; i++) {
+    let item = speicher[i];
+    if (item === undefined) continue;
+    werte += `<div>Letze Runde ${formatTime(item)}</div>`;
+  }
+  speicherung.innerHTML = werte;
 };
+
+function formatTime(milliseconds) {
+  let minutes = Math.floor(milliseconds / 60000);
+  let seconds = Math.floor((milliseconds % 60000) / 1000);
+  let millis = milliseconds % 1000;
+
+  // Führende Nullen hinzufügen, falls nötig
+  minutes = minutes.toString().padStart(2, "0");
+  seconds = seconds.toString().padStart(2, "0");
+  millis = millis.toString().padStart(3, "0");
+
+  return `${minutes}:${seconds}:${millis}`;
+}
